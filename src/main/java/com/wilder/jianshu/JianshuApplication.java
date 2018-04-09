2fc59c92125c2586b25acec1c8945c727876ab4a
WilderGao <845956752@qq.com>
2018-04-09 22:45:32 +0800

    Initial commit

---
+++ src/main/java/com/wilder/jianshu/JianshuApplication.java
@@ -0,0 +1,12 @@
+package com.wilder.jianshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianshuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JianshuApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wilder/jianshu/websocket/HandShake.java
@@ -0,0 +1,9 @@
+package com.wilder.jianshu.websocket;
+
+/**
+ * @author:Wilder Gao
+ * @time:2018/4/9
+ * @Discription：
+ */
+public class HandShake {
+}
